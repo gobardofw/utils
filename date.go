f86f1bc0419594e9698d69639a4c0abe14560825
M Ekramy <m.ajamekramy@gmail.com>
2020-10-06 13:23:37 +0330

    fix jalali date utils

--- date.go
+++ date.go
@@ -7,50 +7,46 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jalaali/go-jalaali"
-	"github.com/mavihq/persian"
+	ptime "github.com/yaa110/go-persian-calendar"
 )
-
-// StringToTime parse string date to date
-func StringToTime(format string, date string) (time.Time, error) {
-	t, err := time.Parse(format, date)
-	if err != nil {
-		return time.Time{}, err
-	}
-	return t, nil
-}
 
 // JalaliToTime convert jalali date string to time from 2006-01-02 format
 func JalaliToTime(jDate string) (time.Time, error) {
+	t := time.Time{}
 	rx := regexp.MustCompile(`^\d{4}(-|\/)(\d{1,2})(-|\/)(\d{1,2})$`)
 	if !rx.MatchString(jDate) {
-		return time.Time{}, errors.New("Inalid date format")
+		return t, errors.New("Inalid date format")
 	}
 
 	jDate = strings.ReplaceAll(jDate, "/", "-")
+	jDate = strings.ReplaceAll(jDate, "\\", "-")
+	jDate = strings.ReplaceAll(jDate, ".", "-")
 	parts := strings.Split(jDate, "-")
 
-	year, _ := strconv.Atoi(parts[0])
-	month, _ := strconv.Atoi(parts[1])
-	day, _ := strconv.Atoi(parts[2])
-	if !jalaali.IsValidDate(year, month, day) {
-		return time.Time{}, errors.New("Inalid date")
+	year, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return t, errors.New("invalid year")
 	}
 
-	y, m, d, err := jalaali.ToGregorian(year, jalaali.Month(month), day)
-	if err != nil {
-		return time.Time{}, err
+	month, err := strconv.Atoi(parts[1])
+	if err != nil || (month > 12 || month < 1) {
+		return t, errors.New("invalid month")
 	}
-	t := time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.UTC)
-	return t, nil
+
+	day, err := strconv.Atoi(parts[2])
+	if err != nil || (month > 31 || month < 1) {
+		return t, errors.New("invalid day")
+	}
+
+	pt := ptime.Date(year, ptime.Month(month), day, 0, 0, 0, 0, ptime.Iran())
+	if pt.Year() != year || int(pt.Month()) != month || pt.Day() != day {
+		return t, errors.New("Inalid date")
+	}
+
+	return pt.Time(), nil
 }
 
 // TimeToJalali convert time to jalali date
-func TimeToJalali(t time.Time, format string) (string, error) {
-	jDate := jalaali.From(t)
-	date, err := jDate.JFormat(format)
-	if err != nil {
-		return "", err
-	}
-	return persian.ToEnglishDigits(date), nil
+func TimeToJalali(t time.Time) (ptime.Time, error) {
+	return ptime.Unix(t.Unix(), t.UnixNano(), ptime.Iran()), nil
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,4 @@ module github.com/gobardofw/utils
 
 go 1.15
 
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/jalaali/go-jalaali v0.0.0-20200831101426-11d9c8c19878
-	github.com/kr/text v0.2.0 // indirect
-	github.com/mavihq/persian v0.0.0-20180720112018-07dac0a5e732
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
-)
+require github.com/yaa110/go-persian-calendar v0.5.0
